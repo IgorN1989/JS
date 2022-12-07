2cdc49d1c3a0b2599d4f89d0692dd5de37451570
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2022-12-07 23:45:44 +0200

    up

---
+++ js/02-timer.js
@@ -0,0 +1,39 @@
+const refs = {
+  input: document.querySelector('#datetime-picker'),
+  startButton: document.querySelector('button'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     console.log(selectedDates[0]);
+//   },
+// };
+
+// flatpickr(refs.input, options);
