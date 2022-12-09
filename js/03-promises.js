f9b6d42e4b03403ec3c3aa1e740f27edb4ea3a3c
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2022-12-09 20:47:09 +0200

    add promises

--- js/03-promises.js
+++ js/03-promises.js
@@ -1,8 +1,32 @@
-function createPromise(position, delay) {
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+
+
+const promise = new Promise((resolve, reject) => {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+
+  setTimeout(() => {
+    if (shouldResolve) {
+      resolve("Success! Value passed to resolve function");
+    } else {
+      reject("Error! Error passed to reject function");
+    }
+  }, 2000)
+})
+
+
+console.log(promise);
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
