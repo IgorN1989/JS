586336a27e683c5f9571d7910728bf563404037c
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2022-12-09 04:14:06 +0200

    Update 02-timer.js

--- js/02-timer.js
+++ js/02-timer.js
@@ -1,3 +1,7 @@
+// import flatpickr from 'flatpickr';
+// import 'flatpickr/dist/flatpickr.min.css';
+// require('flatpickr/dist/themes/material_green.css');
+
 const refs = {
   input: document.querySelector('#datetime-picker'),
   startBtn: document.querySelector('button'),
@@ -9,47 +13,72 @@ const refs = {
 
 refs.startBtn.disabled = true;
 
-console.log();
+refs.startBtn.addEventListener('click', onStartBtnClick)
 
-// refs.startBtn.addEventListener('click', onStartBtnClick);
+function onStartBtnClick() {
+  refs.startBtn.disabled = true;
+  refs.input.disabled = true;
+  countdownTimer.start();
 
+}
 
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    this.refs = {
+      days: document.querySelector('[data-days]'),
+      hours: document.querySelector('[data-hours]'),
+      minutes: document.querySelector('[data-minutes]'),
+      seconds: document.querySelector('[data-seconds]'),
+    };
+  }
 
-// class Timer {
-//   constructor(rootSelector, targetDate) {
-//     this.rootSelector = rootSelector;
-//     this.targetTime = targetTime;
-//   }
+  getDeltaTime(endtime) {
+    const deltaTime = this.convertMs(
+      Date.parse(endtime) - Date.parse(new Date())
+    );
+    const { days, hours, minutes, seconds } = deltaTime;
+    return { deltaTime, days, hours, minutes, seconds };
+  }
 
-//   start() {
-//     const intervalId = setInterval(() => {
+  updateTimer({ days, hours, minutes, seconds }) {
+    this.refs.days.textContent = days;
+    this.refs.hours.textContent = hours;
+    this.refs.minutes.textContent = minutes;
+    this.refs.seconds.textContent = seconds;
+  }
 
-//       const leftedTime = this.targetTime - new Date()
-//     }, 1000);
-//     console.log(leftedTime);
-//     console.log(intervalId);
+  start() {
+    const timer = this.getDeltaTime(this.targetDate);
 
-    
-//   }
-// }
+    this.updateTimer(timer);
 
+    setInterval(() => {
+      const timer = this.getDeltaTime(this.targetDate);
+      this.updateTimer(timer);
+    }, 1000);
+  }
 
+  convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
 
+    const days = this.pad(Math.floor(ms / day));
+    const hours = this.pad(Math.floor((ms % day) / hour));
+    const minutes = this.pad(Math.floor(((ms % day) % hour) / minute));
+    const seconds = this.pad(
+      Math.floor((((ms % day) % hour) % minute) / second)
+    );
+    return { days, hours, minutes, seconds };
+  }
 
-
-
-
-
-// const timer = new Timer (refs.input, targetTime)
-
-// function onStartBtnClick() {
-//   refs.startBtn.disabled = true;
-//   refs.input.disabled = true;
-//   timer.start()
-
-
-// }
-
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+}
 
 
 const options = {
@@ -57,51 +86,26 @@ const options = {
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  // onClose(selectedDates) {
-  //   console.log(selectedDates[0]);
-  //   let targetTime = selectedDates[0];
-  //   if (currentTime > targetTime) {
-  //     window.alert("Please choose a date in the future");
-  //     return;
-  //   } else {
-  //     refs.startBtn.disabled = false;
-  //   }
-  // },
+  onClose(selectedDates) {
+    if (selectedDates[0] < new Date()) {
+      window.alert("Please choose a date in the future");
+      return;      
+    }
+    refs.startBtn.disabled = false;
+    return selectedDates[0];
+  },
+
 };
 
-function convertMs(ms) {
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  const days = Math.floor(ms / day);
-  const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
-
-
-flatpickr(refs.input, options)
+flatpickr(refs.input, options);
 
 
 
 
-
-// console.log(targetTime);
-// console.log(new Date(refs.input.value));
-
-const intervalId = setInterval(() => {
-  const targetTime = new Date(refs.input.value);
-  
-  const leftedTime = targetTime - new Date();
-    console.log(convertMs(leftedTime));
-    }, 1000);
-  
-
-
+const countdownTimer = new CountdownTimer({
+  selector: '#datetime-picker',
+  targetDate: selectedDates[0],
+});
 
 
 
