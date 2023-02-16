c8cb5cf1bd4a9892087a12fd82fe124cea7f7e51
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-02-16 21:27:38 +0200

    les-1

---
+++ js/lesson-1.js
@@ -0,0 +1,219 @@
+// let message1 = 'JavaScript is awesome!';
+// let toUpperCaseMessage1 = message1.toUpperCase();
+// console.log(message1.length);
+// console.log(toUpperCaseMessage1);
+
+// const userYearOfBirth = 1989;
+// console.log(userYearOfBirth);
+
+// let userAge = 32;
+// userAge = 33;
+// console.log(userAge);
+
+// const userName = 'Igor';
+// console.log(userName);
+
+// const COLOR_TEAL = '#009688'
+// const LOGIN_SUCCESS_MESSAGE = 'Ласкаво просимо!';
+
+
+// // Number
+// const age = 20;
+// const points = 15.8;
+
+// // String
+// const username = "Mango";
+// const description = "Javascript для початківців";
+
+// // Boolean
+// const isLoggedIn = true;
+// const canMerge = false;
+// const hasChildren = true;
+// const isModalOpen = false;
+
+// // Null
+// let selectedProduct = null;
+
+// // Undefined
+// let username1;
+// console.log(username1);
+
+// // typeof
+// let username2;
+// console.log(typeof username2);
+
+// let inputValue = null;
+// console.log(typeof inputValue);
+
+// const quantity = 17;
+// console.log(typeof quantity);
+
+// const message2 = "JavaScript is awesome!";
+// console.log(typeof message2);
+
+// const isSidebarOpen = false;
+// console.log(typeof isSidebarOpen);
+
+// // Виведення даних
+// const message3 = "Javascript is awesome!"
+// console.log(message3);
+
+// const username3 = 'Mango';
+// console.log('Username is', username3);
+
+// const alertMessage = "ALERT!!!"
+// alert(alertMessage);
+
+// // Отримання даних
+// const isComing = confirm('Please confirm hotel reservation');
+// console.log(isComing);
+
+// const hotelName = prompt("Please enter desired hotel name");
+// console.log(hotelName);
+
+
+// const value = prompt("Please enter a number!");
+// console.log(typeof value);
+// console.log(value);
+
+// // Math
+// const x = 10;
+// const y = 5;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+
+// let value1 = 10;
+// value1 += 5;
+// console.log(value1);
+
+// // Порівняння
+// const x = 5;
+// const y = 10;
+// const z = 5;
+
+// console.log("x > y:", x > y);
+// console.log("x < y:", x < y);
+// console.log("x < z:", x < z);
+// console.log("x <= z:", x <= z);
+// console.log("x === y:", x === y);
+// console.log("x === z:", x === z);
+// console.log("x !== y:", x !== y);
+// console.log("x !== z:", x !== z);
+
+
+// console.log(5 == "5");
+// console.log(5 != "5");
+// console.log(1 == true);
+// console.log(1 != true);
+
+// // Числа
+// const valueA = "5";
+// console.log(Number(valueA));
+// console.log(typeof Number(valueA));
+
+// const valueB = "random string";
+// console.log(Number(valueB));
+// console.log(typeof Number(valueB));
+
+// console.log(Number.parseInt("5px"));
+// console.log(Number.parseInt("12qwe74"));
+
+// console.log(Number.parseFloat("5px"));
+// console.log(Number.parseFloat("12.46qwe74"));
+
+// const validNumber = Number("51");
+// console.log(Number.isNaN(validNumber));
+
+// const invalidNumber = Number("qweqwe");
+// console.log(typeof invalidNumber);
+// console.log(Number.isNaN(invalidNumber));
+
+// console.log(0.17 + 0.24);
+// console.log((0.17 * 100 + 0.24 * 100) / 100);
+// console.log((0.17 + 0.24).toFixed(2));
+// console.log(typeof (0.17 + 0.24).toFixed(2));
+
+// console.log(Math.floor(1.7));
+// console.log(Math.ceil(3.4));
+// console.log(Math.round(1.45));
+// console.log(Math.round(1.5));
+// console.log(Math.max(10, 5, 2, 24, 33, 7));
+// console.log(Math.min(10, 5, 2, 24, 33, 7));
+// console.log(Math.pow(2, 10));
+// console.log((Math.random() * (10 - 1) + 1).toFixed(0));
+
+// Рядки
+// const message = "Mango " + "is" + " happy";
+// console.log(message);
+
+// console.log(2 + "3");
+// console.log(2 + "2" + 4);
+// console.log(2 + 4 + "3");
+
+// const questName = "Mango";
+// const roomNumber = 207;
+// let greeting = "Welcome " + questName + ", your room number is " + roomNumber + "!";
+// console.log(greeting);
+
+// greeting = `Welcome +${questName}, your room number is ${roomNumber}!`;
+// console.log(greeting);
+
+// const message = "Welcome to Bahamas!";
+// console.log(message.length);
+// console.log(message.toLowerCase());
+// console.log(message.toUpperCase());
+
+// const BRAND_NAME = "SAMSUNG";
+// const userInput = prompt("Please, enter brand name");
+// console.log(BRAND_NAME === userInput);
+
+// const normalizedToUpperCaseInput = userInput.toUpperCase();
+// console.log(BRAND_NAME === normalizedToUpperCaseInput);
+
+// console.log(message.indexOf("B"));
+// console.log(message.indexOf("hello"));
+// console.log(message.indexOf("TO"));
+
+// console.log(message.includes("Wel"));
+// console.log(message.includes("wel"));
+
+// const jsFileName = "script.js"
+// console.log(jsFileName.endsWith("js"));
+
+// const minifiedJsFileName = jsFileName.replace("js", "min.js");
+// console.log(minifiedJsFileName);
+
+// const message = "Welcome to Bahamas!";
+// const newMessage = message.replaceAll("a", "A");
+// console.log(newMessage);
+
+
+// const productName = "Repair droid";
+// console.log(productName.slice(0, 5));
+// console.log(productName.slice(5,productName.length));
+
+// Логічні оператори
+// const age = 33;
+
+// console.log(age > 10 && age < 40);
+// console.log(age > 20 && age < 30);
+
+// console.log(age > 10 || age < 30);
+// console.log(age <20 || age > 80);
+
+
+// REPETA
+
+
+
+
+
+
+
+
+
+
