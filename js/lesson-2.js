76ee3cf414f96b010e8a3d2490d8a6b627eace80
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-04-26 01:00:23 +0300

    Update lesson-2.js

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -8,6 +8,8 @@
 
 // console.log(cost);
 
+// ===================================================
+
 // let cost;
 // // const subscription = 'free';
 // const subscription = 'pro'
@@ -19,6 +21,8 @@
 // }
 
 // console.log(cost);
+
+// ===================================================
 
 // let cost;
 // const subscription = 'premium';
@@ -33,6 +37,8 @@
 //     console.log('Invalid subscription type');
 // }
 // console.log(cost);
+
+// ===================================================
 
 // let type;
 // const age = 20;
@@ -61,15 +67,15 @@
 //     case 'free':
 //         cost = 0;
 //         break;
-    
+
 //     case 'pro':
 //         cost = 100;
 //         break;
-    
+
 //     case 'premium':
 //         cost = 500;
 //         break;
-    
+
 //     default:
 //         console.log('Invalid subscription type');
 // }
@@ -91,7 +97,6 @@
 //     clientCounter += 1;
 // }
 
-
 // let password = '';
 
 // do {
@@ -103,7 +108,6 @@
 // for (let i = 0; i <= 20; i += 5) {
 //     console.log(i);
 // }
-
 
 // const target = 3;
 // let sum = 0;
@@ -130,83 +134,167 @@
 
 // console.log('Лог після циклу');
 
+// const number = 10;
 
-const number = 10;
+// for (let i = 0; i < number; i += 1) {
+//     if (i % 2 === 0) {
+//         continue
+//     }
+//     console.log('Непарне i', i);
+// }
 
-for (let i = 0; i < number; i += 1) {
-    if (i % 2 === 0) {
-        continue
-    }
-    console.log('Непарне i', i);
+// ====================================================================
+// Repeta
+// ====================================================================
+
+// const stars = 21;
+// let price;
+
+// if (stars === 1) {
+//     price = 20;
+// } else if (stars === 2) {
+//     price = 30;
+// } else if (stars === 3) {
+//     price = 50;
+// } else if (stars === 4) {
+//     price = 70;
+// } else if (stars === 5) {
+//     price = 120;
+// } else {
+// console.log("Такої кількості зірок нема");
+// }
+
+// console.log("Price: ", price);
+
+// ====================================================================
+
+// const stars = 21;
+// let price;
+
+// switch (stars) {
+//     case 1:
+//         price = 20;
+//         break;
+
+//     case 2:
+//         price = 30;
+//         break;
+
+//     case 3:
+//         price = 50;
+//         break;
+
+//     case 4:
+//         price = 70;
+//         break;
+
+//     case 5:
+//         price = 120;
+//         break;
+
+//     default:
+//         console.log("Такої кількості зірок нема");
+// }
+// console.log("Price: ", price);
+
+// ====================================================================
+
+// const stars = 6;
+// let price;
+
+// if (stars === 1 || stars === 2) {
+//     price = 20;
+// } else if (stars === 3 || stars === 4) {
+//     price = 30;
+// } else if (stars === 5) {
+//     price = 120;
+// } else {
+// console.log("Такої кількості зірок нема");
+// }
+
+// console.log("Price: ", price);
+
+// ====================================================================
+
+// const stars = 4;
+// let price;
+
+// switch (stars) {
+//   case 1:
+//   case 2:
+//     price = 20;
+//     break;
+
+//   case 3:
+//   case 4:
+//     price = 30;
+//     break;
+
+//   case 5:
+//     price = 120;
+//     break;
+
+//   default:
+//     console.log('Такої кількості зірок нема');
+// }
+// console.log('Price: ', price);
+
+// ====================================================================
+
+// const options = 2;
+
+// let message;
+
+// switch (options) {
+//     case 1:
+//         message = "Ви можете забрати товар завтра з 12:00 в офісі";
+//         break;
+
+//     case 2:
+//         message = "Кур'єр доставить замовлення завтра";
+//         break;
+
+//     case 3:
+//         message = "Замовлення буде надіслано сьогодні";
+//         break;
+
+//     default:
+//         message = "Вам передзвонить менеджер";
+// }
+
+// console.log(message);
+
+// ====================================================================
+
+// for (let i = 0; i < 10; i += 1) {
+// console.log(i);
+// }
+
+// console.log('qweqwe');
+
+// ====================================================================
+
+const minSalary = 500;
+const maxSalary = 5000;
+
+const employees = 5;
+let totalSalary = 0;
+
+for (let i = 1; i <= employees; i += 1) {
+    const salary = Math.round(
+        Math.random() * (maxSalary - minSalary) + minSalary);
+    console.log(`Salare of worker ${i} - ${salary}`);
+    totalSalary += salary;
 }
 
+console.log(totalSalary);
 
+// ====================================================================
 
+// ====================================================================
 
+// ====================================================================
 
+// ====================================================================
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// ====================================================================
