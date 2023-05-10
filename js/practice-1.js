a6a5cec6ca1cc7ea3acf9f91bd31c457cabbeba5
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-05-10 16:40:33 +0300

    Update practice-1.js

--- js/practice-1.js
+++ js/practice-1.js
@@ -1,10 +1,27 @@
-console.log(Number(null));
-console.log(null);
+// console.log(Number(null));
+// console.log(null);
 
-const string = 'hello';
-const arr = [1,2,3];
-console.log(Number(arr));
+// const string = 'hello';
+// const arr = [1,2,3];
+// console.log(Number(arr));
 
 
-console.log(5 + '5');
-console.log('5'+5);
+// console.log(5 + '5');
+// console.log('5'+5);
+
+
+// const answer = prompt('Яка офіційна мова JavaScript?');
+// const correctAnswer = 'ECMAScript';
+
+// console.log(answer);
+
+// if (answer === null) {
+//     alert("Ви не ввели відповідь");
+// } else if (answer.toLowerCase() === correctAnswer.toLowerCase()) {
+//     alert("Вірно!");
+// } else {
+//     alert("Не знаєте? ECMAScript" )
+// }
+
+
+const userInput = prompt('Яка офіційна мова JavaScript?');
