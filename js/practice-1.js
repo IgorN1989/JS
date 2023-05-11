4deb13a5d2b920b8df9d7d3376e71748c5560a3c
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-05-11 13:17:40 +0300

    Update practice-1.js

--- js/practice-1.js
+++ js/practice-1.js
@@ -5,10 +5,10 @@
 // const arr = [1,2,3];
 // console.log(Number(arr));
 
-
 // console.log(5 + '5');
 // console.log('5'+5);
 
+// ====================================================================
 
 // const answer = prompt('Яка офіційна мова JavaScript?');
 // const correctAnswer = 'ECMAScript';
@@ -20,8 +20,31 @@
 // } else if (answer.toLowerCase() === correctAnswer.toLowerCase()) {
 //     alert("Вірно!");
 // } else {
-//     alert("Не знаєте? ECMAScript" )
+//     alert("Не знаєте? ECMAScript")
 // }
 
+// const userInput = prompt('Яка офіційна мова JavaScript?');
+// console.log(userInput);
 
-const userInput = prompt('Яка офіційна мова JavaScript?');
+// if (userInput === 'ECMAScript') {
+//   alert('Вірно!');
+// } else {
+//   alert('Не знаєте? ECMAScript');
+// }
+
+// ====================================================================
+
+// const userInput = Number(prompt('Введіть кількість хвилин'));
+// // console.log(userInput);
+
+// let hours = Math.floor(userInput / 60);
+// hours = String(hours).padStart(2, '0');
+// // console.log(hours);
+
+// let minutes = userInput % 60;
+// minutes = String(minutes).padStart(2, '0');
+// // console.log(minutes);
+
+// console.log(`${hours}:${minutes}`);
+
+// ====================================================================
