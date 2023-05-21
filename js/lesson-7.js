e60fbbc18c3effeb7747944e1ec9e24ced786f12
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-05-21 21:12:17 +0300

    Update lesson-7.js

--- js/lesson-7.js
+++ js/lesson-7.js
@@ -96,39 +96,149 @@
 
 // ====================================================================
 
-const makeDish = function (sheffName, dish) {
-  console.log(`${sheffName} готує ${dish}`);
+// const makeDish = function (sheffName, dish) {
+//   console.log(`${sheffName} готує ${dish}`);
+// };
+
+// // makeDish('Mango', 'пиріжок');
+// // makeDish('Mango', 'омлет');
+// // makeDish('Mango', 'чай');
+
+// // makeDish('Poly', 'котлета');
+// // makeDish('Poly', 'суп');
+// // makeDish('Poly', 'кава');
+
+// const makeSheff = function (name) {
+//   const makeDish = function (dish) {
+//     console.log(`${name} готує ${dish}`);
+//     };
+
+//     return makeDish;
+// };
+
+// const mango = makeSheff('Mango');
+
+// // console.log(mango);
+
+// mango('котлети');
+// mango('пиріжок');
+
+// const poly = makeSheff('Poly');
+
+// poly('кава');
+// poly('суп');
+
+// ====================================================================
+
+// const floatingPoint = 3.456789;
+// const someInt = Math.round(floatingPoint);
+// const withDecimals = Number(floatingPoint.toFixed(2));
+
+// // const rounder = function (number, places) {
+// //   return Number(number.toFixed(places));
+// // };
+
+// // console.log(rounder(3.4567, 2));
+// // console.log(rounder(3.4567, 3));
+// // console.log(rounder(5.1234, 2));
+// // console.log(rounder(3.4567, 3));
+
+// const rounder = function(places) {
+//   return function (num) {
+//     return Number(num.toFixed(places));
+//   };
+// }
+
+// const rounder2 = rounder(2);
+// const rounder3 = rounder(3);
+
+// console.log(rounder2(3.4567));
+// console.log(rounder3(3.4567));
+// console.log(rounder2(5.1234));
+// console.log(rounder3(3.4567));
+
+// ====================================================================
+
+// const salaryManagerFactory = function (employeeName, baseSalary) {
+//   let salary = baseSalary;
+
+//   return {
+//     raise(amount) {
+//       if (amount > 1000) {
+//         return 'What!?';
+//       }
+
+//       salary += amount;
+//     },
+
+//     lower(amount) {
+//       salary -= amount;
+//     },
+
+//     current() {
+//       return `Поточна зарплатня ${employeeName} - ${salary}`;
+//     },
+//   };
+// };
+
+// const salaryManager = salaryManagerFactory('Mango', 5000);
+
+// // console.log(salaryManager);
+// console.log(salaryManager.current());
+// console.log(salaryManager.raise(10000000));
+// console.log(salaryManager.current());
+// console.log(salaryManager.raise(100));
+// console.log(salaryManager.current());
+
+// ====================================================================
+
+// const add = function (a, b, c) {
+//   // console.log(a, b, c);
+//   console.log(arguments);
+//   return a + b + c;
+// };
+
+// const addArrow = (...args) => {
+//   // console.log(a, b, c);
+//   console.log(args);
+//   // return a + b + c;
+// };
+
+// const addArrow1 = (a, b, c) => a + b + c;
+
+// console.log(add(5, 10, 15));
+// console.log(addArrow(5, 10, 15));
+
+// ====================================================================
+
+// const fnA = function () {
+//   return {
+//     a: 5,
+//   };
+// };
+
+// console.log(fnA());
+
+// const arrowFnA = () => ({ arrowA: 5, });
+
+// console.log(arrowFnA());
+
+// ====================================================================
+
+const onGetPositionSuccess = (position) => {
+  console.log(('Це виклик onGetPositionSuccess'));
+  console.log(position);
 };
 
-// makeDish('Mango', 'пиріжок');
-// makeDish('Mango', 'омлет');
-// makeDish('Mango', 'чай');
-
-// makeDish('Poly', 'котлета');
-// makeDish('Poly', 'суп');
-// makeDish('Poly', 'кава');
-
-const makeSheff = function (name) {
-  const makeDish = function (dish) {
-    console.log(`${name} готує ${dish}`);
-    };
-    
-    return makeDish;
+const onGetPositionError = (error) => {
+  console.log('Це виклик onGetPositionError');
+  console.log(error);
 };
 
-const mango = makeSheff('Mango');
-
-// console.log(mango);
-
-mango('котлети');
-mango('пиріжок');
-
-// ====================================================================
-// ====================================================================
-
-// ====================================================================
-// ====================================================================
-// ====================================================================
+window.navigator.geolocation.getCurrentPosition(
+  onGetPositionSuccess,
+  onGetPositionError
+);
 
 // ====================================================================
 // ====================================================================
