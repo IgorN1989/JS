e140d835fc007bc224b5d3e023be96069e044c56
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-05-27 16:32:13 +0300

    filter

--- js/lesson-8.js
+++ js/lesson-8.js
@@ -327,6 +327,10 @@
 // console.log(uniqueSortedCourses);
 
 // ====================================================================
+// ====================================================================
+// ====================================================================
+// ====================================================================
+// ====================================================================
 
 // const numbers = [5, 10, 15, 20, 25];
 
@@ -385,9 +389,70 @@
 // console.table(updatedPlayers1);
 
 // ====================================================================
-// ====================================================================
-// ====================================================================
-// ====================================================================
-// ====================================================================
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// const filteredNumbers = numbers.filter(number => number < 10 || number > 15);
+
+// console.log(filteredNumbers);
+// console.log('numbers', numbers);
+
 // ====================================================================
 
+// const players = [
+//   { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+//   { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+//   { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+//   { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+//   { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+// ];
+
+// const onlinePlayers = players.filter(player => player.online);
+// console.table(onlinePlayers);
+
+// const offlinePlayers = players.filter(player => !player.online);
+// console.table(offlinePlayers);
+
+// ====================================================================
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// const number = numbers.find(number => number === 10);
+// console.log(number);
+
+// ====================================================================
+
+// const players = [
+//   { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+//   { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+//   { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+//   { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+//   { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+// ];
+
+// const playerIdToFind = 'player-3';
+// const playerWithId = players.find(player => player.id === playerIdToFind);
+// console.log(playerWithId);
+
+// const playerNameToFind = 'Poly';
+// const playerWithName = players.find(player => player.name === playerNameToFind);
+// console.log(playerWithName);
+
+// const findPlayerById = (allPlayers, playerId) => {
+//     return allPlayers.find(player => player.id === playerId);
+// };
+
+// console.log(findPlayerById(players, 'player-4'));
+// console.log(findPlayerById(players, 'player-5'));
+
+// ====================================================================
+
+
+
+
+
+
+
+
+// ====================================================================
+// ====================================================================
