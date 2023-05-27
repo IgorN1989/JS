ca8176ab6a5578531acf0a81b4518ca1e9e1e72e
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-05-27 14:55:10 +0300

    map

--- js/lesson-8.js
+++ js/lesson-8.js
@@ -325,4 +325,69 @@
 //   .sort((a, b) => a.localeCompare(b));
 
 // console.log(uniqueSortedCourses);
-  
+
+// ====================================================================
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// numbers.forEach(function (number, index, array) {
+//     console.log(('number', number));
+// });
+
+// console.log(numbers);
+
+// ====================================================================
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// const doubledNums = numbers.map(number => number * 2);
+// console.log(doubledNums);
+
+// ====================================================================
+
+// const players = [
+//   { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+//   { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+//   { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+//   { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+//   { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+// ];
+
+// const playersNames = players.map(({ name }) => name);
+// console.log('playersNames', playersNames);
+
+// const playersIds = players.map(({ id }) => id);
+// console.log('playersIds', playersIds);
+
+// const updatedPlayers = players.map(player => {
+//   console.log(player);
+//   return { ...player, points: (player.points * 110) / 100 };
+// });
+
+// console.table(updatedPlayers);
+
+// const playerIdToUpdate = 'player-3';
+
+// // const updatedPlayers1 = players.map(player => {
+// //   if (playerIdToUpdate === player.id) {
+// //     return { ...player, timePlayed: player.timePlayed + 100 };
+// //   }
+
+// //   return player;
+// // });
+
+// const updatedPlayers1 = players.map(player =>
+//     playerIdToUpdate === player.id
+//         ? { ...player, timePlayed: player.timePlayed + 100 }
+//         : player,
+// );
+
+// console.table(updatedPlayers1);
+
+// ====================================================================
+// ====================================================================
+// ====================================================================
+// ====================================================================
+// ====================================================================
+// ====================================================================
+
