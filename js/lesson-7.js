5d81a8eb7193bf38cbac57b51fbd994e33cf77fc
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-05-27 11:56:27 +0300

    Update lesson-7.js

--- js/lesson-7.js
+++ js/lesson-7.js
@@ -439,28 +439,89 @@
 
 // ====================================================================
 
-const each = (array, callback) => {
-  const newArr = [];
-  for (const el of array) {
-    newArr.push(callback(el));
-  }
-  return newArr;
-};
+// const each = (array, callback) => {
+//   const newArr = [];
+//   for (const el of array) {
+//     newArr.push(callback(el));
+//   }
+//   return newArr;
+// };
 
-console.log(each([64, 49, 36, 25, 16], value => value * 2));
+// console.log(each([64, 49, 36, 25, 16], value => value * 2));
 
-console.log(each([64, 49, 36, 25, 16], value => value - 10));
+// console.log(each([64, 49, 36, 25, 16], value => value - 10));
 
-console.log(each([64, 49, 36, 25, 16], value => Math.sqrt(value)));
+// console.log(each([64, 49, 36, 25, 16], value => Math.sqrt(value)));
 
-console.log(each([1.5, 2.1, 16.4, 9.7, 11.3], value => Math.ceil(value)));
+// console.log(each([1.5, 2.1, 16.4, 9.7, 11.3], value => Math.ceil(value)));
 
-console.log(each([1.5, 2.1, 16.4, 9.7, 11.3], value => Math.floor(value)));
+// console.log(each([1.5, 2.1, 16.4, 9.7, 11.3], value => Math.floor(value)));
 
 // ====================================================================
+
+// const logItems = items => {
+//   console.log(items);
+
+//   for (let i = 0; i < items.length; i += 1) {
+//     console.log(`${i + 1} - ${items[i]}`);
+//   }
+
+// logItems(['Mango', 'Poly', 'Ajax']);
+// logItems(['🍎', '🍇', '🍑', '🍌', '🍋']);
+
 // ====================================================================
+
+// const logItems = items =>
+//   items.forEach((item, idx) => {
+//     console.log(`${idx + 1} - ${item}`);
+//   });
+
+// logItems(['Mango', 'Poly', 'Ajax']);
+// logItems(['🍎', '🍇', '🍑', '🍌', '🍋']);
+
 // ====================================================================
+
+// const printContactInformation = ({ names, phones }) => {
+//   const nameList = names.split(',');
+//   const phoneList = phones.split(',');
+
+//   // for (let i = 0; i < nameList.length; i += 1) {
+//   //   console.log(`${nameList[i]} : ${phoneList[i]}`);
+//   // }
+//   nameList.forEach((name, idx) => {
+// console.log(`${name} - ${phoneList[idx]}`);
+//   })
+// }
+
+// printContactInformation({
+//   names: 'Jacob,William,Solomon,Artemis',
+//   phones: '89001234567,89001112233,890055566377,890055566300',
+// });
+
 // ====================================================================
+
+// function calculateAverage(...args) {
+//   let total = 0;
+//   for (let i = 0; i < args.length; i += 1) {
+//     total += args[i];
+//   }
+//   return total / args.length;
+// }
+
+// console.log(calculateAverage(1, 2, 3, 4));
+// console.log(calculateAverage(14, 8, 2));
+// console.log(calculateAverage(27, 43, 2, 8, 36));
+
+// const calculateAverage = (...args) => {
+//   let total = 0;
+//   args.forEach(el => (total += el));
+//   return total / args.length;
+// };
+
+// console.log(calculateAverage(1, 2, 3, 4));
+// console.log(calculateAverage(14, 8, 2));
+// console.log(calculateAverage(27, 43, 2, 8, 36));
+
 // ====================================================================
 // ====================================================================
 // ====================================================================
