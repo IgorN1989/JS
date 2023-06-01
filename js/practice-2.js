e630689275e89cbfff8d33a9727fe35e70b4dc1f
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-06-02 00:58:38 +0300

    Update practice-2.js

--- js/practice-2.js
+++ js/practice-2.js
@@ -47,3 +47,46 @@
 // console.log(genres);
 
 // ====================================================================
+
+// const genres = ['Jazz', 'Blues', "Rock'N'Roll", 'Country', 'Reggy'];
+
+// function getLongestWord(arr) {
+//   let longestWord = arr[0];
+//   for (let i = 0; i < arr.length; i += 1) {
+//     if (arr[i].length > longestWord.length) {
+//       longestWord = arr[i];
+//     }
+//   }
+//   return longestWord;
+// }
+
+// console.log(getLongestWord(genres));
+
+// let longestWord = genres[0];
+// for (let i = 0; i < genres.length; i += 1) {
+//   if (genres[i].length > longestWord.length) {
+//     longestWord = genres[i];
+//   }
+// }
+
+// console.log(longestWord);
+
+// ====================================================================
+
+// const managerSalaries = [1000, 1500, 2500, 4000, 5000];
+// const developerSalaries = [8000, 40000, 15000];
+
+// const allSalaries = [...managerSalaries, ...developerSalaries];
+// console.log(allSalaries);
+
+// let total = 0;
+// for (const salary of allSalaries) {
+//     total += salary;
+// }
+
+// const totalAmount = allSalaries.reduce((acc, item) => acc += item, 0)
+
+// console.log(total);
+// console.log(totalAmount);
+
+// ====================================================================
