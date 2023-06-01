4114df718ade508478413fbe7bd3520ab27ec8ce
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-06-01 23:46:44 +0300

    Update practice-2.js

---
+++ js/practice-2.js
@@ -0,0 +1,49 @@
+// const checkString = (str) => {
+//     let exp;
+//     if (str && 0) {
+//         exp = false;
+//     } else {
+//         exp = true;
+//     }
+//     console.log(exp);
+// }
+
+// checkString('HTML & CSS & JS Tasks');
+// console.log(Boolean('string' && 0));
+
+// const a = {};
+// const b = {};
+
+// const c = {
+//     d: 1,
+//     c: 2,
+// };
+
+// c[a] = 3;
+// c[b] = 4;
+
+// console.log(c);
+
+// const arr = [1, 2, 3, 4, 5]
+
+// console.log(arr.push(6, 7, 8));
+// console.log(arr);
+
+// const arr = [];
+
+// console.log(arr.pop());
+
+// ====================================================================
+
+// const genres = ['Jazz', 'Blues'];
+
+// genres.push("Rock'n'Roll");
+// console.log(genres);
+// console.log(genres[0]);
+// console.log(genres[genres.length - 1]);
+// console.log(genres.shift());
+// console.log(genres);
+// genres.unshift('Country', 'Reggy');
+// console.log(genres);
+
+// ====================================================================
