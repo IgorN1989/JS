e4d818c90ec53e316d89fc87365f0dc971e508a2
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-06-06 23:55:15 +0300

    Update lesson-9.js

--- js/lesson-9.js
+++ js/lesson-9.js
@@ -61,3 +61,142 @@
 // ====================================================================
 
 // ====================================================================
+
+// function foo() {
+//     console.log(this);
+// }
+
+// foo();
+
+// const obj = {
+//     logCtx: foo,
+// }
+
+// console.log(obj.logCtx);
+// obj.logCtx()
+
+// ====================================================================
+
+// const objA = {
+//     name: "objA",
+//     logCtx() {
+//         console.log(this);
+//     }
+// };
+
+// objA.logCtx();
+
+// const objB = {
+//     name: 'objB',
+//     foo: objA.logCtx,
+// };
+
+// console.log(objB.foo);
+// objB.foo()
+
+// ====================================================================
+
+// const foo = () => { console.log(this); }
+// foo();
+
+// ====================================================================
+
+// const obj = {
+//     objThis: this,
+//     foo: () => {
+//         console.log(this);
+//     }
+// }
+
+// // console.log(obj.objThis);
+// obj.foo();
+
+// ====================================================================
+
+// const foo = () => {
+//     console.log(this);
+// }
+
+// const obj = {
+//     key: foo,
+// }
+
+// obj.key();
+
+// ====================================================================
+
+// const chopShop = {
+//   stones: [
+//     {
+//       name: 'Emerald',
+//       price: 1300,
+//       quantity: 4,
+//     },
+//     {
+//       name: 'Diamond',
+//       price: 2700,
+//       quantity: 3,
+//     },
+//     {
+//       name: 'Sapphire',
+//       price: 1400,
+//       quantity: 7,
+//     },
+//     {
+//       name: 'Ruby',
+//       price: 800,
+//       quantity: 2,
+//     },
+//   ],
+//   //   calcTotalPrice(stoneName) {
+//   //     return this.stones.reduce((acc, { name, price, quantity }) => {
+//   //       if (name === stoneName) {
+//   //         return price * quantity;
+//   //       }
+//   //       return acc;
+//   //     }, 0);
+//   //   },
+
+// //   calcTotalPrice(stoneName) {
+// //     return this.stones.reduce(
+// //       (acc, { name, price, quantity }) => (name === stoneName ? (acc = price * quantity) : acc),
+// //       0
+// //     );
+// //   },
+
+//     calcTotalPrice(stoneName) {
+//         const {price, quantity} = this.stones.find(item => item.name === stoneName)
+//         return price * quantity;
+//     }
+// };
+
+// console.log(chopShop.calcTotalPrice('Emerald'));
+// console.log(chopShop.calcTotalPrice('Diamond'));
+// console.log(chopShop.calcTotalPrice('Sapphire'));
+// console.log(chopShop.calcTotalPrice('Ruby'));
+
+// ====================================================================
+
+const phonebook = {
+  contacts: [],
+  add(contact) {
+    const newContact = {
+      list: 'default',
+      ...contact,
+      id: this.generateId(),
+      createdAt: this.getDate(),
+    };
+    this.contacts.push(newContact);
+  },
+  generateId() {
+    return '-' + Math.random().toString(36).substr(2, 9);
+  },
+  getDate() {
+    return Date.now();
+  },
+};
+
+console.log(phonebook.add({ name: 'Mango', email: 'mango@mail.com', list: 'friends' }));
+
+// ====================================================================
+// ====================================================================
