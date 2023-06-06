68f1d65e1369fd74e0f47573109e41dcc1ee7657
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-06-07 00:06:42 +0300

    Update lesson-9.js

--- js/lesson-9.js
+++ js/lesson-9.js
@@ -177,26 +177,35 @@
 
 // ====================================================================
 
-const phonebook = {
-  contacts: [],
-  add(contact) {
-    const newContact = {
-      list: 'default',
-      ...contact,
-      id: this.generateId(),
-      createdAt: this.getDate(),
-    };
-    this.contacts.push(newContact);
-  },
-  generateId() {
-    return '-' + Math.random().toString(36).substr(2, 9);
-  },
-  getDate() {
-    return Date.now();
-  },
-};
+// const phonebook = {
+//   contacts: [],
+//   add(contact) {
+//     const newContact = {
+//       list: 'default',
+//       ...contact,
+//       id: this.generateId(),
+//       createdAt: this.getDate(),
+//     };
+//     this.contacts.push(newContact);
+//     return this.contacts;
+//   },
+//   generateId() {
+//     return '_' + Math.random().toString(36).substr(2, 9);
+//   },
+//   getDate() {
+//     return Date.now();
+//   },
+// };
 
-console.log(phonebook.add({ name: 'Mango', email: 'mango@mail.com', list: 'friends' }));
+// // console.log(phonebook.add({ name: 'Mango', email: 'mango@mail.com', list: 'friends' }));
+// // console.log(phonebook.add({ name: 'Poly', email: 'poly@mail.com' }));
+
+// const newPhonebook = {
+//     contacts: [],
+// };
+
+// // phonebook.add.call(newPhonebook, { name: 'Mark', email: 'mark@mail.com', list: 'friends' });
+
 
 // ====================================================================
 // ====================================================================
