a1885aaae967405813d4f4a7978a4869a712b9ce
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-06-25 23:36:32 +0300

    Update tasks.js

--- js/tasks.js
+++ js/tasks.js
@@ -155,19 +155,19 @@
 
 // console.log(rangeOfNumbers(10,20));
 
-function peopleWithAgeDrink(old) {
-  let drink = '';
-  if (old < 14) {
-    drink = 'toddy';
-  } else if (old >= 14 && old < 18) {
-    drink = 'coke';
-  } else if (old >= 18 && old < 21) {
-    drink = 'beer';
-  } else {
-    drink = 'whisky';
-    }
+// function peopleWithAgeDrink(old) {
+//   let drink = '';
+//   if (old < 14) {
+//     drink = 'toddy';
+//   } else if (old >= 14 && old < 18) {
+//     drink = 'coke';
+//   } else if (old >= 18 && old < 21) {
+//     drink = 'beer';
+//   } else {
+//     drink = 'whisky';
+//     }
     
-    return `drink ${drink}`
-}
+//     return `drink ${drink}`
+// }
 
-console.log(peopleWithAgeDrink(13));
+// console.log(peopleWithAgeDrink(13));
