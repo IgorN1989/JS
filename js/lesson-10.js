531ee9fbbefc08d771d18b006cfe12357f2b472c
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-06-27 01:18:16 +0300

    Update lesson-10.js

--- js/lesson-10.js
+++ js/lesson-10.js
@@ -1,29 +1,276 @@
-class User {
-  // Необов'язкове оголошення публічних властивостей
-  name;
-  // Обов'язкове оголошення приватних властивостей
-  #email;
+// class User {
+//   // Необов'язкове оголошення публічних властивостей
+//   name;
+//   // Обов'язкове оголошення приватних властивостей
+//   #email;
 
-  constructor({ name, email }) {
-    this.name = name;
-    this.#email = email;
-  }
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.#email = email;
+//   }
 
-  getEmail() {
-    return this.#email;
-  }
+//   getEmail() {
+//     return this.#email;
+//   }
 
-  changeEmail(newEmail) {
-    this.#email = newEmail;
-  }
-}
+//   changeEmail(newEmail) {
+//     this.#email = newEmail;
+//   }
+// }
 
-const mango = new User({
-  name: "Манго",
-  email: "mango@mail.com",
-});
-mango.changeEmail("mango@supermail.com");
-// console.log(mango.getEmail()); // mango@supermail.com
-// console.log(mango.#email); // Виникне помилка, це приватна властивість
+// const mango = new User({
+//   name: "Манго",
+//   email: "mango@mail.com",
+// });
+// mango.changeEmail("mango@supermail.com");
+// // console.log(mango.getEmail()); // mango@supermail.com
+// // console.log(mango.#email); // Виникне помилка, це приватна властивість
 
-console.log(mango);
+// console.log(mango);
+
+// ====================================================================
+
+// class User {
+//   constructor(name, email) {
+//     this.name = name;
+//     this.email = email;
+//   }
+// }
+
+// const mango = new User('Mango', 'mango@mail.com');
+// console.log(mango);
+
+// const poly = new User('Poly', 'poly@mail.com');
+// console.log(poly);
+
+// ====================================================================
+
+// class User {
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.email = email;
+//   }
+// };
+
+// const mango = new User({
+//   name: 'Mango',
+//   email: 'mango@mail.com',
+// });
+// console.log(mango);
+
+// const poly = new User({
+//   name: 'Poly',
+//   email: 'poly@mail.com',
+// });
+// console.log(poly);
+
+// ====================================================================
+
+// class User {
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.email = email;
+//   }
+
+//   getEmail() {
+//     return this.email;
+//   }
+
+//   changeEmail(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+
+// console.log(User);
+
+// ====================================================================
+
+// class User {
+//   name;
+//   #email;
+
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.#email = email;
+//   }
+
+//   getEmail() {
+//     return this.#email;
+//   }
+
+//   changeEmail(newEmail) {
+//     this.#email = newEmail;
+//   }
+// }
+
+// const mango = new User({
+//   name: 'Mango',
+//   email: 'mango@mail.com',
+// });
+
+// console.log(mango.getEmail());
+// mango.changeEmail('newmango@mail.com');
+// console.log(mango.getEmail());
+// console.log(mango.email);
+// console.log(mango.#email);
+
+// ====================================================================
+
+// class User {
+//   #email;
+
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.#email = email;
+//   }
+
+//   get email() {
+//     return this.#email;
+//   }
+
+//   set email(newEmail) {
+//     if (newEmail === '') {
+//       console.log('Error!!!!!');
+//       return;
+//     }
+
+//     this.#email = newEmail;
+//   }
+// }
+
+// const mango = new User({ name: 'Mango', email: 'mango@mail.com' });
+// console.log(mango.email);
+
+// mango.email = 'mango@supermail.com';
+// console.log(mango.email);
+
+// mango.email = '';
+// console.log(mango.email);
+
+// ====================================================================
+
+// class User {
+//   static Roles = {
+//     ADMIN: 'admin',
+//     EDITOR: 'editor',
+//   };
+
+//   #email;
+//   #role;
+
+//   constructor({ email, role }) {
+//     this.#email = email;
+//     this.#role = role;
+//   }
+
+//   get role() {
+//     return this.#role;
+//   }
+
+//   set role(newRole) {
+//     this.#role = newRole;
+//   }
+// }
+
+// const mango = new User({ email: 'mango@mail.com', role: User.Roles.ADMIN, });
+
+// console.log(mango.Roles);
+// console.log(User.Roles);
+
+// console.log(mango.role);
+// mango.role = User.Roles.EDITOR;
+// console.log(mango.role);
+
+// ====================================================================
+
+// class User {
+//   static #takenEmails = [];
+
+//   static isEmailTaken(email) {
+//     return User.#takenEmails.includes(email);
+//   }
+
+//   #email;
+
+//   constructor({ email }) {
+//     this.#email = email;
+//     User.#takenEmails.push(email);
+//   }
+// }
+
+// const mango = new User({ email: 'mango@mail.com' });
+
+// console.log(User.isEmailTaken('poly@mail.com'));
+// console.log(User.isEmailTaken('mango@mail.com'));
+
+// ====================================================================
+
+// class User {
+//   #email;
+
+//   constructor(email) {
+//     this.#email = email;
+//   }
+
+//   get email() {
+//     return this.#email;
+//   }
+
+//   set email(newEmail) {
+//     this.#email = newEmail;
+//   }
+// }
+
+// class ContentEditor extends User {
+// }
+
+// const editor = new ContentEditor('mango@mail.com');
+// console.log(editor);
+// console.log(editor.email);
+
+// class Admin extends User { };
+
+// const admin = new Admin('poly@mail.com')
+// console.log(admin);
+
+// ====================================================================
+
+// class User {
+//   #email;
+
+//   constructor(email) {
+//     this.#email = email;
+//   }
+
+//   get email() {
+//     return this.#email;
+//   }
+
+//   set email(newEmail) {
+//     this.#email = newEmail;
+//   }
+// }
+
+// class ContentEditor extends User {
+//   constructor({ email, posts }) {
+//     super(email);
+//     this.posts = posts;
+//   }
+
+//   addPost(post) {
+//     this.posts.push(post);
+//   }
+// }
+
+// const editor = new ContentEditor({ email: 'mango@mail.com', posts: [] });
+// console.log(editor);
+// console.log(editor.email);
+// editor.addPost('post-1');
+// console.log(editor.posts);
+
+// ====================================================================
+
+
+// ====================================================================
+
+// ====================================================================
