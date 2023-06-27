e0dc241b3c82ed261fca17b92223512750cbc4ee
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-06-27 23:14:19 +0300

    Update lesson-10.js

--- js/lesson-10.js
+++ js/lesson-10.js
@@ -270,7 +270,102 @@
 
 // ====================================================================
 
+// const objB = {
+//     b: 2,
+// }
+
+// const objA = {
+//     a: 1,
+//     __proto__: objB,
+// }
+
+// console.log(objA);
+// console.log(objA.b);
 
 // ====================================================================
 
+// const objC = {
+//     z: 5,
+// };
+
+// console.log('objC', objC);
+// console.log('objC', objC.hasOwnProperty('z'));
+
+// const objB = Object.create(objC);
+// objB.y = 2;
+
+// console.log('objB', objB);
+// console.log('objB',objB.y);
+// console.log('objB',objB.z);
+
+// const objA = Object.create(objB);
+// objA.x = 1;
+
+// console.log('objA', objA);
+// console.log('objA', objA.x);
+// console.log('objA', objA.y);
+// console.log('objA', objA.z);
+
+// objA.z = 1000;
+// console.log('objA', objA);
+// console.log('objC', objC);
+
 // ====================================================================
+
+// const Car = function ({ brand, model, price } = {}) {
+//   //   console.log(config);
+//   //   const { brand, model, price } = config;
+//   this.brand = brand;
+//   this.model = model;
+//   this.price = price;
+
+//   //   this.changePrice = function (newPrice) {
+//   //     this.price = newPrice;
+//   //   };
+// };
+
+// Car.prototype.sayHi = function () {
+//   console.log('Car.prototype.sayHi -> this', this);
+//   console.log('Hello ;)');
+// };
+
+// Car.prototype.changePrice = function (newPrice) {
+//   this.price = newPrice;
+// };
+
+// console.log(Car.prototype);
+
+// const myCar = new Car({ brand: 'Audi', model: 'Q3', price: 35000 });
+// console.log(myCar);
+// myCar.sayHi();
+// myCar.changePrice(10000);
+// console.log(myCar);
+
+// const myCar2 = new Car({ brand: 'BMW', model: 'X6', price: 50000 });
+// console.log(myCar2);
+// myCar2.sayHi();
+
+// const myCar3 = new Car({ brand: 'Audi', model: 'A6', price: 60000 });
+// console.log(myCar3);
+// myCar3.sayHi();
+
+// const myCar4 = new Car();
+// console.log(myCar4);
+// myCar4.sayHi();
+
+// ====================================================================
+
+// const User = function ({ email, password } = {}) {
+//   this.email = email;
+//   this.password = password;
+// };
+
+// User.prototype.changeEmail = function (newEmail) {
+//   this.email = newEmail;
+// };
+
+// const mango = new User({ email: 'mango@MediaList.com', password: '1111111' });
+// console.log(mango);
+
+// mango.changeEmail('my-new-email@mail.com');
+// console.log(mango);
