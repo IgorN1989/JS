ba843f92a02555c319f3311456bdccc6dc11173b
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-06-30 01:50:37 +0300

    Update lesson-10.js

--- js/lesson-10.js
+++ js/lesson-10.js
@@ -540,3 +540,343 @@
 // ajax.gainXp(300);
 
 // ====================================================================
+
+// const obj = {
+//   name: 'user',
+//   sayHello() {
+//     return 'Hello';
+//   },
+// };
+
+// // console.log(obj);
+
+// const user = Object.create(obj);
+
+// user.age = 18;
+
+// // console.log(user);
+// // console.log(user.sayHello());
+
+// const admin = Object.create(user);
+
+// console.log(admin.age);
+// console.log(admin);
+
+// admin.sayHello = function () {
+//   return 'Hello from ADMIN!!!!!';
+// };
+
+// console.log(admin);
+// console.log(admin.sayHello());
+
+// ====================================================================
+
+// class User {
+//   static Roles = {
+//     ADMIN: 'Admin',
+//     USER: 'User',
+//   };
+
+//   static total = 0;
+
+//   static counter() {
+//     User.total += 1;
+//     console.log(User.total);
+//   }
+
+//   #password;
+//   constructor(val1, val2, val3, val4) {
+//     this.name = val1;
+//     this.email = val2;
+//     this.#password = val3;
+//     this.role = val4;
+//     User.counter();
+//   }
+
+//   get mail() {
+//     console.log(this.email);
+//   }
+
+//   set mail(newEmail) {
+//     if (this.email !== newEmail) {
+//       this.email = newEmail;
+//     }
+//   }
+
+//   get password() {
+//     return this.#password;
+//   }
+
+//   set password(newPassword) {
+//     this.#checkPassword(newPassword);
+//   }
+
+//   #checkPassword(newPassword) {
+//     if (newPassword.length > 5) {
+//       this.#password = newPassword;
+//     } else {
+//       console.log('Потрібно більше символів');
+//     }
+//   }
+// }
+
+// const objA = new User('User', 'user@mail.com', '12345', User.Roles.USER);
+// console.log(objA);
+// // console.log(objA.mail);
+// // objA.mail = 'test@mail.com';
+// // console.log(objA);
+
+// const objB = new User('Admin', 'admin@mail.com', '12345', User.Roles.ADMIN);
+// // console.log(objB);
+// // objB.password = '123';
+// // objB.password = '1234567890';
+// console.log(objB);
+
+// const objC = new User('Gamer', 'gamer@mail.com', '12345', User.Roles.USER);
+// console.log(objC);
+
+// ====================================================================
+
+// class Blogger {
+//   constructor({ name, age, numberOfPosts, topics }) {
+//     this.name = name;
+//     this.age = age;
+//     this.numberOfPosts = numberOfPosts;
+//     this.topics = topics;
+//     }
+
+//     getInfo() {
+//         return `User ${this.name} is ${this.age} years old and has ${this.numberOfPosts} posts`
+//     }
+
+//     updatePostCount(value) {
+//         this.numberOfPosts += value;
+//     }
+// }
+
+// const mango = new Blogger({
+//   name: 'mango@mail.com',
+//   age: 24,
+//   numberOfPosts: 20,
+//   topics: ['tech', 'cooking'],
+// });
+
+// console.log(mango);
+// console.log(mango.getInfo());
+// mango.updatePostCount(5);
+// console.log(mango.getInfo());
+
+// ====================================================================
+
+// class Storage {
+//   constructor(arr) {
+//     this.items = arr;
+//     }
+
+//   getItems() {
+//     return this.items;
+//     }
+
+//     addItem(item) {
+//         this.items.push(item);
+//     }
+
+//     removeItem(item) {
+//         if (this.items.includes(item)) {
+//             const idx = this.items.indexOf(item);
+//             this.items.splice(idx, 1);
+//         }
+//     }
+// }
+
+// const storage = new Storage(['🍎', '🍋', '🍇', '🍑']);
+// // console.log(storage);
+
+// storage.addItem('🍌');
+// // console.log(storage);
+
+// storage.removeItem('🍎');
+// // console.log(storage);
+
+// ====================================================================
+
+// class User {
+//     #login
+//     #email
+
+//     constructor({login, email}) {
+//         this.#login = login;
+//         this.#email = email;
+//     }
+
+//     get login() {
+//         return this.#login;
+//     }
+
+//     set login(newLogin) {
+//         this.#login = newLogin;
+//     }
+
+//       get email() {
+//         return this.#email;
+//     }
+
+//     set email(newEmail) {
+//         this.#email = newEmail;
+//     }
+// }
+
+// const mango = new User({ login: 'Mango', email: 'mango@dog.woof' });
+
+// console.log(mango);
+// mango.login
+// mango.login = 'Poly';
+// console.log(mango);
+
+// mango.email;
+// mango.email = 'poly@MediaList.com';
+// console.log(mango);
+
+// ====================================================================
+
+// class Notes {
+//   static Priority = {
+//     LOW: 'low',
+//     NORMAL: 'normal',
+//     HIGH: 'high',
+//   };
+
+//   constructor(arr) {
+//     this.items = arr;
+//   }
+
+//   addNote(note) {
+//     this.items.push(note);
+//   }
+
+//   //   removeNote(text) {
+//   //     const idx = this.items.findIndex(({ text: itemText }) => itemText === text);
+//   //     console.log(idx);
+//   //     if (!!~idx) {
+//   //       this.items.splice(idx, 0);
+//   //     } else {
+//   //         console.log('not found');
+//   //     }
+//   //   }
+
+//   removeNote(text) {
+//     const inArr = this.items.some(item => item.text === text);
+//     if (inArr) {
+//       const idx = this.items.findIndex(({ text: itemText }) => itemText === text);
+//       this.items.splice(idx, 0);
+//     } else {
+//       console.log('not found');
+//     }
+//   }
+
+//     updatePriority(text, newPriority) {
+//         const inArr = this.items.find(item => item.text === text);
+//         if (inArr) {
+//             inArr.priority = newPriority;
+//         } else {
+//             console.log('not found');
+//         }
+//   }
+// }
+
+// const myNotes = new Notes([]);
+// myNotes.addNote({ text: 'Моя перша нотатка', priority: Notes.Priority.LOW });
+// console.log(myNotes.items);
+
+// myNotes.addNote({ text: 'Моя друга нотатка', priority: Notes.Priority.NORMAL });
+// console.log(myNotes.items);
+
+// myNotes.removeNote('Моя перша нотатка');
+// console.log(myNotes.items);
+
+// myNotes.updatePriority('Моя друга нотатка', Notes.Priority.HIGH);
+// console.log(myNotes.items);
+
+// ====================================================================
+
+// class Toggle {
+//   // on: false
+//   constructor({ isOpen = false } = {}) {
+//     this.on = isOpen;
+//   }
+// }
+
+// const firstToggle = new Toggle({ isOpen: true });
+// console.group(firstToggle);
+// console.log(firstToggle.on);
+// // firstToggle.toggle();
+// console.log(firstToggle.on);
+// console.groupEnd(firstToggle);
+
+// const secondToggle = new Toggle();
+// console.group(secondToggle);
+// console.log(secondToggle.on);
+// // secondToggle.toggle();
+// console.log(secondToggle.on);
+// console.groupEnd(secondToggle);
+
+// ====================================================================
+
+// class User {
+//   static Roles = {
+//     ADMIN: 'Admin',
+//     USER: 'User',
+//   };
+
+//   static total = 0;
+
+//   static counter() {
+//     User.total += 1;
+//     console.log(User.total);
+//   }
+
+//   #password;
+//   constructor(val1, val2, val3, val4) {
+//     this.name = val1;
+//     this.email = val2;
+//     this.#password = val3;
+//     this.role = val4;
+//     User.counter();
+//   }
+
+//   get mail() {
+//     console.log(this.email);
+//   }
+
+//   set mail(newEmail) {
+//     if (this.email !== newEmail) {
+//       this.email = newEmail;
+//     }
+//   }
+
+//   get password() {
+//     return this.#password;
+//   }
+
+//   set password(newPassword) {
+//     this.#checkPassword(newPassword);
+//   }
+
+//   #checkPassword(newPassword) {
+//     if (newPassword.length > 5) {
+//       this.#password = newPassword;
+//     } else {
+//       console.log('Потрібно більше символів');
+//     }
+//   }
+// }
+
+// class Admin extends User {
+//   constructor(name, email, password, role, icon) {
+//     super(name, email, password, role);
+//     this.icon = icon;
+//   }
+// }
+
+// const objA = new Admin('Admin', 'admin@mail.com', '12345', User.Roles.ADMIN, 'cool icon');
+// console.log(objA);
