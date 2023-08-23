615339ef41f11a8e0cdb37aae70304ab576d31ac
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-08-24 02:29:25 +0300

    Update lesson-17.js

--- js/lesson-17.js
+++ js/lesson-17.js
@@ -439,3 +439,67 @@
 // console.timeEnd('for');
 
 // ======================================================
+// Практика
+// Потрібно створити два приклади годинника (Електронний та механічний)
+// const arrDay = ['Неділя', 'Понеділок', 'Вівторок', 'Середа', 'Четвер', 'П`ятниця', 'Субота'];
+
+// const namesOfMonth = [
+//   'Січень',
+//   'Лютий',
+//   'Березень',
+//   'Квітень',
+//   'Травень',
+//   'Червень',
+//   'Липень',
+//   'Серпень',
+//   'Вересень',
+//   'Жовтень',
+//   'Листопад',
+//   'Грудень',
+// ];
+
+// const elements = {
+//   day: document.querySelector('.date-day'),
+//   date: document.querySelector('.date'),
+//   month: document.querySelector('.date-month'),
+//   year: document.querySelector('.date-year'),
+//   digitalClock: document.querySelector('.digital-clock'),
+//   secondsArrow: document.querySelector('.clock-seconds__arrow'),
+//   minutesArrow: document.querySelector('.clock-minutes__arrow'),
+//   hoursArrow: document.querySelector('.clock-hours__arrow'),
+// };
+
+// const id = setInterval(() => {
+//   const currentTime = new Date();
+//   const current = {
+//     day: arrDay[currentTime.getDay()],
+//     date: currentTime.getDate(),
+//     month: namesOfMonth[currentTime.getMonth()],
+//     year: currentTime.getFullYear(),
+//     hours: currentTime.getHours(),
+//     minutes: currentTime.getMinutes(),
+//     seconds: currentTime.getSeconds(),
+//     miliseconds: currentTime.getUTCMilliseconds(),
+//   };
+
+//   elements.day.textContent = current.day;
+//   elements.date.textContent = current.date;
+//   elements.month.textContent = current.month;
+//   elements.year.textContent = current.year;
+//   elements.digitalClock.textContent = `Поточний час
+//   ${formatTime(current.hours)}:
+//   ${formatTime(current.minutes)}:
+//   ${formatTime(current.seconds)}`;
+
+//   const secondsDeg = (360 / 60) * current.seconds + (360 / 60 / 1000) * current.miliseconds;
+//   const minutesDeg = (360 / 60) * current.minutes + (360 / 60 / 60) * current.seconds;
+//   const hoursDeg = (360 / 12) * current.hours + (360 / 12 / 60) * current.minutes;
+
+//   elements.secondsArrow.style.transform = `rotate(${secondsDeg}deg)`;
+//   elements.minutesArrow.style.transform = `rotate(${minutesDeg}deg)`;
+//   elements.hoursArrow.style.transform = `rotate(${hoursDeg}deg)`;
+// }, 10);
+
+// function formatTime(value) {
+//   return value.toString().padStart(2, '0');
+// }
