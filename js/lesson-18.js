a47c6071966920913556b34d0a99887e1388941c
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-08-25 02:36:05 +0300

    Update lesson-18.js

--- js/lesson-18.js
+++ js/lesson-18.js
@@ -88,4 +88,162 @@
 // ========================================================
 // ========================================================
 
-// const promise = new Promise()
+// const promise = new Promise((resolve, reject) => {
+//   const canFulfill = Math.random() > 0.5;
+
+//   setTimeout(() => {
+//     if (canFulfill) {
+//       resolve('Проміс виконався з результатом (виконано, fulfilled)');
+//     }
+
+//     reject('Проміс виконався з помилкою (відхилено, rejected)');
+//   }, 500);
+// });
+
+// console.log(promise);
+
+// // promise.then(
+// //   result => {
+// //     console.log(`✅ ${result}`);
+// //   },
+// //   error => {
+// //     console.log(`❌ ${error}`);
+// //   }
+// // );
+
+// promise.then(onFulfilled, onRejected);
+
+// function onFulfilled(result) {
+//   console.log('onFulfilled -> onFulfilled');
+//   console.log(`✅ ${result}`);
+// }
+
+// function onRejected(error) {
+//   console.log('onRejected -> onRejected');
+//   console.log(`❌ ${error}`);
+// }
+
+// ========================================================
+
+// const promise = new Promise((resolve, reject) => {
+//   const canFulfill = Math.random() > 0.5;
+
+//   setTimeout(() => {
+//     if (canFulfill) {
+//       resolve('Проміс виконався з результатом (виконано, fulfilled)');
+//     }
+
+//     reject('Проміс виконався з помилкою (відхилено, rejected)');
+//   }, 500);
+// });
+
+// // promise.then(result => {
+// //   console.log(result);
+
+// //   return 5;
+// // })
+// //   .then(x => {
+// //     console.log(x);
+
+// //     return 10;
+// //   })
+// //   .then(y => {
+// //   console.log(y);
+// // })
+
+// promise
+//   .then(onFulfilled)
+//   .then(x => {
+//     console.log(x);
+
+//     return 10;
+//   })
+//   .then(y => console.log(y))
+//   .catch(error => console.log(error))
+//   .finally(() => console.log('Буде виконано в будь-якому випадку'));
+
+// function onFulfilled(result) {
+//   console.log('onFulfilled -> onFulfilled');
+//   console.log(`✅ ${result}`);
+// }
+
+// function onRejected(error) {
+//   console.log('onRejected -> onRejected');
+//   console.log(`❌ ${error}`);
+// }
+
+// ========================================================
+
+// const makeOrder = (dish, onSuccess, onError) => {
+//   const DELAY = 500;
+
+//   const passed = Math.random() > 0.5;
+
+//   setTimeout(() => {
+//     if (passed) {
+//       onSuccess('Ось ваша страва')
+//     } else {
+//       onError('Вибачьте, страви не має')
+//     }
+//     return 5;
+//   }, DELAY)
+// };
+
+// makeOrder('пиріжек', onMakeOrderSuccess, onMakeOrderError)
+
+// function onMakeOrderSuccess(result) {
+//   console.log('onMakeOrderSuccess');
+//   console.log(result);
+// }
+
+// function onMakeOrderError(error) {
+//   console.log('onMakeOrderSuccess');
+//   console.log(error);
+// }
+
+// const makeOrder = dish => {
+//   const DELAY = 500;
+
+//   return new Promise((resolve, reject) => {
+//     const passed = Math.random() > 0.5;
+//     setTimeout(() => {
+//       if (passed) {
+//         resolve(`✅Ось ваша страва ${dish}`);
+//       }
+
+//       reject(`❌Вибачьте, страви ${dish} не має`);
+//     }, DELAY);
+//   });
+// };
+
+// makeOrder('пиріжек').then(onMakeOrderSuccess).catch(onMakeOrderError);
+
+// function onMakeOrderSuccess(result) {
+//   console.log('onMakeOrderSuccess');
+//   console.log(result);
+// }
+
+// function onMakeOrderError(error) {
+//   console.log('onMakeOrderSuccess');
+//   console.log(error);
+// }
+
+// ========================================================
+
+// const makeOrder = dish => {
+//   return Promise.resolve(`✅Ось ваша страва ${dish}`)
+// };
+
+// makeOrder('пиріжек').then(onMakeOrderSuccess)
+
+// function onMakeOrderSuccess(result) {
+//   console.log('onMakeOrderSuccess');
+//   console.log(result);
+// }
+
+// function onMakeOrderError(error) {
+//   console.log('onMakeOrderSuccess');
+//   console.log(error);
+// }
+
+// ========================================================
