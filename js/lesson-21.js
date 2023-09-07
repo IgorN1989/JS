5556833380da76ba976356c78933c5d9bdf9df7b
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-09-08 01:22:08 +0300

    Update lesson-21.js

--- js/lesson-21.js
+++ js/lesson-21.js
@@ -49,3 +49,29 @@
 //   .catch(error => console.log('ERROR' + error));
 
 // ==================================================================
+
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//   .then(response => response.json())
+//   .then(json => console.log(json));
+
+// ==================================================================
+
+// fetch('https://jsonplaceholder.typicode.com/posts/1').then(console.log);
+
+// ==================================================================
+
+// const post = {
+//   userId: 105,
+//   title: 'Hello World',
+//   body: 'I love JS',
+// };
+
+// const options = {
+//   method: 'POST',
+//   body: JSON.stringify(post),
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// };
+
+// fetch('https://jsonplaceholder.typicode.com/posts', options);
